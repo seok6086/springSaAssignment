9ca18fbb9f044df9c34594957db45126c1a2e2cf
황보석 <zheoajfl@gmail.com>
2022-11-25 16:48:29 +0900

    필드 생성 및 객체 연습

---
+++ src/Example/Car.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class Car {
+
+    //필드
+
+    String company = "현대자동차";
+    String model = "아이오닉5";
+    String color = "은색";
+    int maxSpeed = 300;
+    int speed;
+}
